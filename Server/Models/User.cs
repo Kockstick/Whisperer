a0155513dc31ccc19fa46f4c92a03f37c3167e96
student <gnil47311@gmail.com>
2023-12-13 15:05:29 +0700

    Adding models

---
+++ Server/Models/User.cs
@@ -0,0 +1,8 @@
+public class User{
+    public int Id {get; set;}
+    public string Login {get;set;}
+    public string Password {get;set;}
+    public string Name {get;set;}
+
+    public int ChatsId {get;set;}
+}
